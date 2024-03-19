c14a3da9a97c05e973983d5de2f0a8a34875f62f
himali <himalir_21ec@sapthagiri.edu.in>
2024-03-19 18:15:45 +0530

    alt1

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -14,10 +14,9 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    
 
-    package_name='differential_robo' #<--- CHANGE ME
+    package_name='navbot' 
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -41,14 +40,14 @@ def generate_launch_description():
 
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
                     launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+   
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
--- launch/navigation_launch.py
+++ launch/navigation_launch.py
@@ -25,7 +25,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('differential_robo')
+    bringup_dir = get_package_share_directory('navbot')
 
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
